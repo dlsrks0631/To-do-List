11f0019f0f41cc3869c5da30c3f7a0a250943dba
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-23 22:11:16 +0900

    Update app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// const -> constant 값이 바뀔 수 없음
+// let -> 업데이트 가능
+
+let a = 5;
+const b = 2;
+const myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
