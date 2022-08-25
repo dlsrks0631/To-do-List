7519209b8fb3e401c8348d060d492869900dc617
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-25 13:20:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const calculator = {
-  add: function (a, b) {
-    console.log(a + b);
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
+const age = parseInt(prompt("How old are you ? "));
+
+if (isNaN(age)) {
+  console.log("Please write a number");
+} else if (age < 18) {
+  console.log("Thank you for writing your age.");
+} else {
+  console.log("You can ");
+}
