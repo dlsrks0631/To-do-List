15691d2cd5e23431ee1355fa28d27b1214634500
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-26 00:25:33 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const h1 = document.querySelector("#hello:first-child h1");
 
 function handleTitleClick() {
   const clickedClass = "clicked";
-  if (h1.className === clickedClass) {
-    h1.className = "";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedList);
   } else {
-    h1.className = clickedClass;
+    h1.classList.add(clickedClass);
   }
+  h1.classList.toogle(clickedClass);
 }
 
 h1.addEventListener("click", handleTitleClick);
