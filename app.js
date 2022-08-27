7ac9aff9e614947b8ffa302eab21758a15db2c8c
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-27 21:21:11 +0900

    Update JS

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const h1 = document.querySelector("#hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+// == const loginForm = document.getElementById("login-form")
+// -> id 를 가져온다는 사실을 알기 때문에
 
-function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedList);
-  } else {
-    h1.classList.add(clickedClass);
-  }
-  h1.classList.toogle(clickedClass);
+// loginForm에서 input과 button을 가져옴
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+// == const loginInput = document.querySelector("#login-form input");
+// == const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+  console.log(loginInput.value);
+  console.log("click");
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
