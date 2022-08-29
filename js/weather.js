94ea497ea01cb2c9f112e4afad1b93f366b55495
dlsrks0631 <65211547+dlsrks0631@users.noreply.github.com>
2022-08-29 23:55:09 +0900

    Update HTML

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,15 @@
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=35.7991768&lon=127.1163561&appid=acabe7976e964119b699ffe087e0db0e&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
